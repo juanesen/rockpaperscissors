5c2c5d6829ab5d8e6c965c564e394cf696b853a3
juanesen <156286782+juanesen@users.noreply.github.com.>
2024-02-12 07:56:54 +0000

    The game is working beach boi
    I had to add a bunch of functions in place for it to run accordingly
    I didn't loop it for now
    
    That is all that I have to say for now.

--- js/script.js
+++ js/script.js
@@ -1,5 +1,6 @@
 // What are we going to do : is a very simple rock, paper, scissors game
 
+let bold = "font-weight: bold";
 
 // machine has one variable where we store their answer
 let machineSelection;
@@ -7,33 +8,75 @@ let machineSelection;
 // user has one variable where we store their answer
 let userSelection;
 
-// machine selects randomly one of the three options available
+// A function for machine to select randomly one of the three options available
 function randomMachineSelection() {
     let options = ["rock", "paper", "scissors"];
     return(options[(Math.floor(Math.random() * options.length))]);
-  }
+  };
 
-// we store the random option inside the machineSelection
-machineSelection = randomMachineSelection();
+// A function to ask user what their selection is, not letting it pick other than rock papers scissors
+function userChoice() {
+     userSelection = prompt("Rock, Paper or Scissors beach boi ?");
+     if (userSelection == null){
+          userChoice()
+     } else {
+          userSelection = userSelection.toLowerCase();
+     }
+     rockPaperScissors();  
+     return(userSelection);
+};
 
-// User is prompt for the option they want
-// User's option is stored in userSelection as no caps 
-userSelection = prompt("Rock, Paper or Scissors beach boi ?");
-userSelectionLowerCase = userSelection.toLowerCase();
-console.log(userSelectionLowerCase);
 
-// only let user enter Rock, Paper or Scissors
-if (userSelectionLowerCase == "rock") {
-     console.log("you have chosen rock")
-} else if (userSelectionLowerCase == "paper") {
-     console.log("you have chosen paper")
-} else if (userSelectionLowerCase == "scissors") {
-     console.log("you have chosen scissors")
-} else {
-    console.log("you didn't choose anything beach boi");
-}
-
-console.log(userSelectionLowerCase);
+// only let user enter Rock, Paper or Scissors and announce the choice
+function rockPaperScissors() {
+     if (userSelection == null) {
+          alert("you didn't choose anything beach boi");
+          userSelection = prompt("Rock, Paper or Scissors beach boi ?");
+          rockPaperScissors();
+     } else if (userSelection == "paper") {
+          return(userSelection = "paper");
+     } else if (userSelection == "scissors") {
+          return(userSelection = "scissors");
+     } else if (userSelection == "rock") {
+          return(userSelection = "rock");
+     }
+          else {
+          alert("you didn't choose anything beach boi");
+          userSelection = prompt("Rock, Paper or Scissors beach boi ?");
+          userSelection = userSelection.toLowerCase();
+          rockPaperScissors();
+     }
+};
 
 
 // once each party has chosen the right one then we see who won 
+function playgame(userSelection, machineSelection) {
+
+     // User's option is stored in userSelection as no caps 
+     userSelection = userChoice();
+     console.log("you have chosen " + userSelection);
+
+     // we store the random option inside the machineSelection
+     machineSelection = randomMachineSelection();
+     console.log("evil machine has chosen " + machineSelection);
+
+     if (userSelection == machineSelection) {
+          console.log("%cThere is no winner in life only losers", bold);
+     } else if (userSelection == "paper" & machineSelection == "rock") {
+          console.log("%csilly user is the winner", bold);
+     } else if (userSelection == "rock" & machineSelection == "scissors") {
+          console.log("%csilly user is the winner", bold);
+     } else if (userSelection == "scissors" & machineSelection == "paper") {
+          console.log("%csilly user is the winner", bold);
+     } else {
+          console.log("%cevil machine is the winner", bold);
+     }
+};
+
+playgame();
+playgame();
+playgame();
+playgame();
+playgame();
+
+
